f323a8a27f9c15a5e144762c96ab06aa2d33c98a
Allison Olshefke <allisonolshefke@allisons-mbp.dhcp.nd.edu>
2016-03-20 23:06:45 -0400

    modified:   Planet.cpp 	modified:   Planet.hpp

--- Planet.cpp
+++ Planet.cpp
@@ -1,16 +1,19 @@
 #include "Planet.hpp"
 
+//constructor
 Planet::Planet(double m, double x, double y, double z)
 {
 	mass=m;
 	pos.set(x,y,z);
 }
 
+//calculate the force
 Tensor Planet::calcForce(Planet p)
 {
 	double G=6.67e-11; 
 	//totalForce=(G*mass*p.getMass())/(pos.distance(p.getPos())*pos.distance(p.getPos()));
 	totalForce.setx((G*mass*p.getMass())/((p.getx()-pos.getx())*(p.getx()-pos.getx())));
+	//this takes care of "equal but opposite"
 	if(pos.getx()>p.getx())
 	{
 		totalForce.setx(totalForce.getx()*-1);
@@ -18,6 +21,7 @@ Tensor Planet::calcForce(Planet p)
 	return totalForce;
 }
 
+//calculate acceleration based on force
 Tensor Planet::calcAccel()
 {
 	accel.setx(totalForce.getx()/mass);
@@ -56,6 +60,7 @@ double Planet::getz()
 	return pos.getz();
 }
 
+//moves the planets according to the force
 void Planet::update(double dt)
 {
 	pos.setx(pos.getx()+vel.getx()*dt);
