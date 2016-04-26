08512a1c92c441a2d40604027acf338e0e70a81b
Chris Scaramella <CScarame@nd.edu>
2016-04-26 17:01:14 -0400

    modified:   Window.cpp 	modified:   Window.h

--- Window.cpp
+++ Window.cpp
@@ -1,9 +1,10 @@
 #include "Window.h"
-#include <sstream>
+
 
 Window::Window(std::string windowname,string inputfile, int i_height,int i_width,int i_bpp)
-			: height(i_height),width(i_width),bpp(i_bpp){
-	//Start SDL
+			: height(i_height),width(i_width),bpp(i_bpp)
+{
+	//Start SDL, TTF
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
 		cout << "SDL_Init() failed: " << SDL_GetError() << endl;
@@ -16,7 +17,8 @@ Window::Window(std::string windowname,string inputfile, int i_height,int i_width
 	// load support for the JPG and PNG image formats
 	int flags=IMG_INIT_JPG|IMG_INIT_PNG;
 	int initted=IMG_Init(flags);
-	if((initted&flags) != flags) {
+	if((initted&flags) != flags)
+	{
 		cout<<"IMG_Init: Failed to init required jpg and png support " << IMG_GetError() << endl;
 	}
 
@@ -39,161 +41,81 @@ Window::Window(std::string windowname,string inputfile, int i_height,int i_width
 		return;
 	}
 	SDL_FreeSurface(loadedimage);
-	Tensor t(0,0);
-	topleft = t;
-	botright= t;
+
 
 	// Inititalize time
 	time = 0;
 
-
 	// Read input file
 	objects = readFile(inputfile);
-	// Create time object for display
-	//time_obj = createobj("Time",12,"fonts/arial.ttf",0,0);
 }
 
-Window::~Window(){
-	/*for(unsigned int i=0;i<obj.size();i++){
-		obj[i]->free();
-	}*/
+Window::~Window()
+{
+	//Cleanup and quit SDL
 	cleanup(objects);
 	TTF_Quit();
 	SDL_Quit();
 }
 
-
-/*int Window::createobj(std::string imagename, Tensor t){
-	Object newobj(imagename,screen,0,0);
-	obj.push_back(&newobj);
-	move(obj.size()-1,t);
-	return obj.size()-1;
-}
-
-int Window::createobj(std::string message, int fontsize,
-											std::string fontname, int x, int y){
-	Object newobj(message,fontsize,fontname,screen,x,y);
-	obj.push_back(&newobj);
-	return obj.size()-1;
-}
-
-void Window::newmessage(int obj_num,std::string message){
-	obj[obj_num]->newmessage(message);
-}
-
-void Window::addObject(Object * o) {
-	obj.push_back(o);
-}
-
-bool Window::removeobj(int obj_num){
-	obj[obj_num]->invisible();
-	return 0;
-}
-
-bool Window::returnobj(int obj_num){
-	obj[obj_num]->revisible();
-	return 0;
-}
-
-bool Window::colorobj(int obj_num,Uint8 r,Uint8 g,Uint8 b){
-	obj[obj_num]->setFontColor(r,g,b);
-	return 0;
-}
-
-bool Window::move(int obj_number, Tensor t){
-	int x = getx(t,obj_number);
-	int y = gety(t,obj_number);
-	obj[obj_number]->move(x,y);
-	return 0;
-}*/
-
-int Window::getheight(){
-	return height;
-}
-
-int Window::getwidth(){
-	return width;
-}
-
-bool Window::display(){
-
-	//reset();
-	for (int i = 0; i < Body::bodies.size(); i++) {
-		Body::bodies[i]->display(screen, topleft, botright, 10, 10);
-	}
-	/*for(unsigned int i = 0;i<obj.size();i++){
-		//obj[i]->update();
-		if (obj[i]->isVisible()) {
-			SDL_BlitSurface(obj[i]->getImage(), NULL, screen, NULL);
-		}
-	}*/
-	//Earth.display(screen, topleft,botright,height,width);
-	//Moon.display(screen, topleft,botright,height,width);
-	SDL_Flip(screen);
-	return 0;
-}
-
-bool Window::reset(){
-	SDL_BlitSurface(background,NULL,screen, NULL);
-	return 0;
-}
-
-void Window::update(int dt){
-	//Earth.update(dt);
-}
-
-void Window::run(){
-	while(!quit){
-		while(SDL_PollEvent(&event)){
+// Updates and displays planets and handles events continuously
+void Window::run()
+{
+	// Continue until program closes
+	while(!quit)
+	{
+		while(SDL_PollEvent(&event))
+		{
 			handle_events();
 		}
-		Planet::update(dt);
-		reset();
-		display();
-		updateTime(dt);
+		if(!pause)
+		{
+			Planet::update(dt);
+			reset();
+			display();
+			updateTime(dt);
+		}
 	}
 }
 
-void Window::handle_events(){
+// Event handling function
+void Window::handle_events()
+{
 	// User closes window
 	if (event.type == SDL_QUIT){
 		quit = true;
 		return;
 	}
+	if (event.type == SDL_KEYDOWN)
+	{
+		switch(event.key.keysym.sym)
+		{
+			case SDLK_p : pause = !pause;
+		}
+	}
 }
 
-/*int Window::getx(Tensor t,int obj_number){
-	double numer = t.getx() - topleft.getx();
-	double denom = botright.getx() - topleft.getx();
-	return (numer/denom * height) - obj[obj_number]->geth();
-}
-int Window::gety(Tensor t, int obj_number){
-	double numer = t.gety() - topleft.gety();
-	double denom = botright.gety() - topleft.gety();
-	return numer/denom * width - obj[obj_number]->getw();
-}*/
-
-void Window::updateTime(double dt)
+// Display functions
+bool Window::display()
 {
-	// Increment time
-	time += dt;
-	int total_seconds = int(time);
+	//reset();
+	for (int i = 0; i < Body::bodies.size(); i++)
+	{
+		Body::bodies[i]->display(screen);
+	}
 
-	// Convert to Years, Months, Days, Hours, Minutes, Seconds
-	int years = total_seconds / 60 / 60 / 24 / 30 / 12;
-	int months = (total_seconds / 60 / 60 / 24 / 30) % 12;
-	int days = (total_seconds / 60 / 60 / 24) % 30;
-	int hours = (total_seconds / 60 / 60) % 24;
-	int mins = (total_seconds / 60) % 60;
-	int secs = total_seconds % 60;
-
-	ostringstream timeconvert;
-	timeconvert << years << " Years, " << months << " Months, " << days << " Days, " << setw(2) << hours << " Hours, " << setw(2) << mins << " Minutes, " << setw(2) << secs << " Seconds";
-	string timestring = timeconvert.str();
-	printText(timestring, "fonts/arial.ttf", 14, {255, 255, 255});
-	//newmessage(time_obj,timestring);
+	SDL_Flip(screen);
+	return 0;
 }
 
+// Return screen to background image
+bool Window::reset()
+{
+	SDL_BlitSurface(background,NULL,screen, NULL);
+	return 0;
+}
+
+// Display message
 void Window::printText(string message, string fontname, int fontsize, SDL_Color color)
 {
 	cout << "Displaying " << message << endl;
@@ -220,9 +142,30 @@ void Window::printText(string message, string fontname, int fontsize, SDL_Color 
 	}
 	SDL_Flip(screen);
 	TTF_CloseFont(font);
-	//SDL_FreeSurface(text);
 }
 
+// Update time passed
+void Window::updateTime(double dt)
+{
+	// Increment time
+	time += dt;
+	int total_seconds = int(time);
+
+	// Convert to Years, Months, Days, Hours, Minutes, Seconds
+	int years = total_seconds / 60 / 60 / 24 / 30 / 12;
+	int months = (total_seconds / 60 / 60 / 24 / 30) % 12;
+	int days = (total_seconds / 60 / 60 / 24) % 30;
+	int hours = (total_seconds / 60 / 60) % 24;
+	int mins = (total_seconds / 60) % 60;
+	int secs = total_seconds % 60;
+
+	ostringstream timeconvert;
+	timeconvert << years << " Years, " << months << " Months, " << days << " Days, " << setw(2) << hours << " Hours, " << setw(2) << mins << " Minutes, " << setw(2) << secs << " Seconds";
+	string timestring = timeconvert.str();
+	printText(timestring, "fonts/arial.ttf", 14, {255, 255, 255});
+}
+
+//Read in body information
 vector<Body *> Window::readFile(string file)
 {
 	vector<Body *> objects;
@@ -264,6 +207,7 @@ vector<Body *> Window::readFile(string file)
 	return objects;
 }
 
+// Cleanup body information
 void Window::cleanup(vector<Body *> objects)
 {
 	for (int i = 0; i < objects.size(); i++)
--- Window.h
+++ Window.h
@@ -7,6 +7,7 @@
 #include "Tensor.h"
 #include "Planet.h"
 #include <string>
+#include <sstream>
 #include <vector>
 #include <iostream>
 #include <iomanip>
@@ -17,25 +18,25 @@ class Window
 	public:
 		Window(std::string windowname, string infile, int i_height=640, int i_width=480, int i_bpp=32);
 		~Window();
-		/*int createobj(std::string imagename, Tensor t);
-		int createobj(std::string message, int fontsize,
-									std::string fontname, int x, int y);
-		bool removeobj(int obj_num);
-		bool returnobj(int obj_num);
-		void addObject(Object *);
-		bool colorobj(int,Uint8,Uint8,Uint8);
-		bool move(int obj_number, Tensor t);*/
-		int getheight();
-		int getwidth();
+
+		// Updates and displays planets and handles events continuously
+		void run();
+
+		// Event handling function
+		void handle_events();
+
+		// Display functions
 		bool display();
 		bool reset();
-		void update(int dt=1);
-		void run();
-		void handle_events();
-		void updateTime(double);
-		void newmessage(int, string);
 		void printText(string, string, int, SDL_Color);
+
+		// Update time passed
+		void updateTime(double);
+
+		//Read in body information
 		vector<Body *> readFile(string);
+
+		// Cleanup body information
 		void cleanup(vector<Body *>);
 	private:
 		SDL_Surface* screen;
@@ -44,16 +45,11 @@ class Window
 		int height;
 		int width;
 		int bpp;
-		//std::vector<Object *> obj;
 		vector<Body *> objects;
-		Tensor topleft;
-		Tensor botright;
-		/*int getx(Tensor,int);
-		int gety(Tensor,int);*/
 		bool quit;
+		bool pause;
 		double time; // time in seconds
 		int dt = 1800;
-		int time_obj;
 };
 
 #endif // WINDOW_H
