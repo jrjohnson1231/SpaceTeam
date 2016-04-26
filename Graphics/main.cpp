492cc1fc36599c9e7aa3fb4a64ffd37e8705d529
Chris Scaramella <CScarame@nd.edu>
2016-04-25 21:29:49 -0400

    new file:   images/Earth.png 	new file:   images/Moon.png 	new file:   images/Space.jpg 	modified:   main.cpp

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -16,14 +16,16 @@
 #include <string>
 #include "../Planet.h"
 
-int main ( int argc, char** argv ){
+int main ( int argc, char** argv )
+{
     Tensor t(-1e24,-1e24);
     Tensor t2(1e24,1e24);
     Tensor t3(0,0);
     Tensor t4(1e22,1e22);
     Window w("Test",t,t2,800,600);
-	Planet Earth("Earth", "images/red_square.jpg", 5.97e24,395,395,0, 0, -10, 0);
-	Planet Moon("Moon", "images/black_square.jpg", 7.33e22,3.84e8 + 400, 395, 0, 0, 1022.828, 0);
+	Planet Earth("Earth", "images/Earth.png", 5.97e24,395,395,0, 0, -10, 0);
+	Planet Moon("Moon", "images/Moon.jpg", 7.33e22,3.84e8 + 400, 395, 0, 
+	0, 1022.828, 0);
 	//Planet Other("Planer 3", "images/black_square.jpg", 1e25, 350, 150);
 	//Planet Thing("Thing", 3.44e23, 5.97e4, 4.83e6, 0);
 	//Object e("images/red_square.jpg", &Earth);
