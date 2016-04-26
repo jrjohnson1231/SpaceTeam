ef00d44aa8e3bb65a9ae22f4cb1cbc88c61a7721
Chris Scaramella <CScarame@nd.edu>
2016-04-25 23:33:52 -0400

    modified:   Window.cpp

--- Graphics/Window.cpp
+++ Graphics/Window.cpp
@@ -99,16 +99,12 @@ bool Window::display(){
 	for (int i = 0; i < Body::bodies.size(); i++) {
 		Body::bodies[i]->display(screen, topleft, botright, 10, 10);
 	}
-	for (unsigned int i = 0; i < obj.size(); i++)
-	{
-		obj[i]->
-	}
-	/*for(unsigned int i = 0;i<obj.size();i++){
+	for(unsigned int i = 0;i<obj.size();i++){
 		//obj[i]->update();
 		if (obj[i]->isVisible()) {
 			SDL_BlitSurface(obj[i]->getImage(), NULL, screen, NULL);
 		}
-	}*/
+	}
 	//Earth.display(screen, topleft,botright,height,width);
 	//Moon.display(screen, topleft,botright,height,width);
 	SDL_Flip(screen);
